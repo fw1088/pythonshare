74e6f9886d7902f5527d85ffa780814047d970dd
fw1088 <fw1088@163.con>
2016-11-30 08:14:21 +0800

    modify server

--- server.py
+++ server.py
@@ -2,26 +2,29 @@
 # -*-coding:utf-8 -*-
 import socket
 
-#本例子演示了基本的socket操作
-#不过有个缺陷，为了保持长连接
-#只能处理一个客户端，如果要处理
-#多个客户端，需要使用多线程和多进程模型
-#或者使用epoll,select,poll模型
-
+hint = '''本例子演示了基本的socket操作
+不过有个缺陷，为了保持长连接
+只能处理一个客户端，如果要处理
+多个客户端，需要使用多线程和多进程模型
+或者使用epoll,select,poll模型
+'''
 HOST = '127.0.0.1'
 PORT = 8001
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((HOST, PORT))
 s.listen(5)
-
+print '*********************************'
+print hint
+print '*********************************'
 print 'Server start at: %s:%s' %(HOST, PORT)
 print 'welcome to meituan'
 print '---**MeiTuan**--'
 print '----------------'
 print 'wait for connection...'
-
+result = 0
 while True:
+    result = 0
     conn, addr = s.accept()
     print 'Connected by ', addr
     while True:   
@@ -31,6 +34,14 @@ while True:
 	if data ==  "exit":
 	    conn.send("exit")
 	    break	
-        conn.send("server received you message.")
-    break
+        try:
+            conn.send("server received you message.")
+        except socket.error, e:
+	    result = 1
+	    break
+    if result == 1:
+	continue
+    else:
+	print 'I will leave,bye'
+	break
 conn.close()
